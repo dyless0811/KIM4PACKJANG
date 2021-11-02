a9c813022e3562e58f9c4692389bec4daf3d69aa
rlawhdduq <rlawhdduq95@gmail.com>
2021-11-02 17:58:09 +0900

    카테고리<대>, <소> 상품, 사이즈, 상품색상 재고 Dto 생성

---
+++ kh8semi4/src/main/java/semi/beans/ProductSizeDto.java
@@ -0,0 +1,29 @@
+package semi.beans;
+
+public class ProductSizeDto {
+	private int no;
+	private int sizeNo;
+	private int productNo;
+	public ProductSizeDto() {
+		super();
+		// TODO 자동 생성된 생성자 스텁
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getSizeNo() {
+		return sizeNo;
+	}
+	public void setSizeNo(int sizeNo) {
+		this.sizeNo = sizeNo;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+}
