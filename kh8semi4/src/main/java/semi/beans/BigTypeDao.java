e42ec93b69ec6721667191fa17605410a3fd7dc5
hongdyun9410 <dkwntbvj@naver.com>
2021-11-05 18:20:48 +0900

    bigType / Product Dao생성 -동현-

---
+++ kh8semi4/src/main/java/semi/beans/BigTypeDao.java
@@ -0,0 +1,93 @@
+package semi.beans;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BigTypeDao {
+	//추가
+	public void insert(BigTypeDto bigtypeDto) throws Exception{
+		Connection con = JdbcUtils.connect();
+		String sql="insert into vlues(no_seq.nextval,?)";
+		
+		PreparedStatement ps =con.prepareStatement(sql);
+		ps.setString(1, bigtypeDto.getName());
+		
+		ps.execute();
+		
+		con.close();	
+	}
+	//수정
+	public boolean update(BigTypeDto bigTypeDto) throws Exception{
+		Connection con=JdbcUtils.connect();
+		String sql="update bigType set name=? where no=?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, bigTypeDto.getName());
+		ps.setInt(2, bigTypeDto.getNo());
+		
+		int result = ps.executeUpdate();
+		
+		con.close();
+		return result>0;	
+	}
+	//삭제
+	public boolean delete(int no)throws Exception{
+		Connection con = JdbcUtils.connect();
+		String sql="delete bigType where no=?";
+		
+		PreparedStatement ps= con.prepareStatement(sql);
+		ps.setInt(1, no);
+		
+		int result = ps.executeUpdate();
+		
+		con.close();
+		return result>0;	
+	}
+	//조회
+	public List<BigTypeDto> list() throws Exception{
+		Connection con = JdbcUtils.connect();
+		String sql="select * from bigType";
+		
+		PreparedStatement ps= con.prepareStatement(sql);
+		
+		ResultSet rs= ps.executeQuery();
+		List<BigTypeDto> list= new ArrayList<>();
+		while(rs.next()) {
+			BigTypeDto bigTypeDto= new BigTypeDto();
+			
+			bigTypeDto.setNo(rs.getInt("no"));
+			bigTypeDto.setName(rs.getString("name"));
+			
+			list.add(bigTypeDto);
+		}
+		con.close();
+		
+		return list;
+	}
+	//단일조회
+	public  BigTypeDto get(int no) throws Exception{
+		Connection con = JdbcUtils.connect();
+		String sql="select * form bigType where no=?";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1,no);
+		ResultSet rs= ps.executeQuery();
+		
+		BigTypeDto bigTypeDto;
+		
+		if(rs.next()) {
+			bigTypeDto=new BigTypeDto();
+			
+			bigTypeDto.setNo(rs.getInt("no"));
+			bigTypeDto.setName(rs.getString("name"));
+		}else {
+			bigTypeDto=null;
+		}
+		
+		con.close();
+		return bigTypeDto;
+	}
+
+}
---
+++ kh8semi4/src/main/java/semi/beans/ProductDao.java
@@ -0,0 +1,148 @@
+package semi.beans;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDao {
+	
+	//상품 등록
+	public void insert(ProductDto productDto) throws Exception{
+		Connection con = JdbcUtils.connect();
+		
+		String sql ="insert into product values(product_seq.nextval,?,?,?,?)";
+		PreparedStatement ps =con.prepareStatement(sql);
+		ps.setInt(1, productDto.getSmallTypeNo());
+		ps.setString(2, productDto.getName());
+		ps.setInt(3, productDto.getPrice());
+		ps.setString(4, productDto.getDescription());
+		
+		
+		ps.execute();
+		
+		con.close();
+		
+	}
+	//상품 수정
+	public boolean update(ProductDto productDto) throws Exception{
+		Connection con= JdbcUtils.connect();
+		
+		String sql="update product set smallTypeNo=? name=? price=? description=? where no=?";
+		
+		PreparedStatement ps =con.prepareStatement(sql);
+		ps.setInt(1, productDto.getSmallTypeNo());
+		ps.setString(2, productDto.getName());
+		ps.setInt(3, productDto.getPrice());
+		ps.setString(4, productDto.getDescription());
+		ps.setInt(5, productDto.getNo());
+				
+		int result= ps.executeUpdate();
+		con.close();
+		
+		return result>0;
+		
+	}
+	
+	//상품 삭제
+	public boolean delete(int no) throws Exception {
+		Connection con = JdbcUtils.connect();
+		
+		String sql="delete product where no=?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, no);
+		
+		int result= ps.executeUpdate();
+		con.close();
+		
+		return result>0;
+	}
+	
+	//상품 조회
+	public List<ProductDto> list() throws Exception{
+		Connection con = JdbcUtils.connect();
+		
+		String sql="select * from product";
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		List<ProductDto> list = new ArrayList<>();
+		while(rs.next()) {
+			ProductDto productDto = new ProductDto();
+			productDto.setNo(rs.getInt("no"));
+			productDto.setSmallTypeNo(rs.getInt("small_type_no"));
+			productDto.setName(rs.getString("name"));
+			productDto.setPrice(rs.getInt("price"));
+			productDto.setDescription(rs.getString("description"));
+			productDto.setViews(rs.getInt("views"));
+			
+			list.add(productDto);
+		}
+		con.close();
+		
+		return list;
+		
+	}
+	//상품 조회(단일조회)
+	public ProductDto get(int no)throws Exception{
+		Connection con = JdbcUtils.connect();
+		String sql="select * form product where no=?";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1,no);
+		ResultSet rs= ps.executeQuery();
+		
+		ProductDto productDto;
+		
+		if(rs.next()) {
+			productDto=new ProductDto();
+			productDto.setNo(rs.getInt("no"));
+			productDto.setSmallTypeNo(rs.getInt("small_type_no"));
+			productDto.setName(rs.getString("name"));
+			productDto.setPrice(rs.getInt("price"));
+			productDto.setDescription(rs.getString("description"));
+			productDto.setViews(rs.getInt("views"));
+			
+			
+		}else {
+			productDto=null;
+		}
+		
+		con.close();
+		return productDto;
+		
+	}
+	//상품검색
+	public List<ProductDto> search(String column, String keyword) throws Exception{
+		Connection con= JdbcUtils.connect();
+		
+		String sql="select * from product where instr(#1,?)>0 order by #1 asc";
+		sql=sql.replace("#1", column);
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs =ps.executeQuery();
+		
+		List<ProductDto> list = new ArrayList<>();
+		while(rs.next()) {
+			ProductDto productDto = new ProductDto();
+			productDto.setNo(rs.getInt("no"));
+			productDto.setSmallTypeNo(rs.getInt("small_type_no"));
+			productDto.setName(rs.getString("name"));
+			productDto.setPrice(rs.getInt("price"));
+			productDto.setDescription(rs.getString("description"));
+			productDto.setViews(rs.getInt("views"));
+			
+			list.add(productDto);
+		}
+		con.close();
+		
+		return list;
+		
+		
+	}
+	
+	
+
+}
