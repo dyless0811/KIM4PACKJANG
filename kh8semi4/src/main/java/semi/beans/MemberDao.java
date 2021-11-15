3fd15d3dea50e7ae5404cae21f1cf6ec88420689
효민 <@112.161.95.54>
2021-11-15 14:41:49 +0900

    memberdao 추가

--- kh8semi4/src/main/java/semi/beans/MemberDao.java
+++ kh8semi4/src/main/java/semi/beans/MemberDao.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public class MemberDao {
 
-	//회원가입 메소드
+	// 회원가입 메소드
 	public void join(MemberDto memberDto) throws Exception {
 		Connection con = JdbcUtils.connect();
-		
+
 		String sql = "insert into member values(?, ?, ?, ?, ?, ?, to_date(?, 'YYYY-MM-DD'), sysdate, 0,'회원',?)";
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, memberDto.getId());
@@ -23,36 +23,37 @@ public class MemberDao {
 		ps.setString(7, memberDto.getBirth());
 		ps.setString(8, memberDto.getGender());
 		ps.execute();
-		
+
 		con.close();
 	}
-	
-	//회원 탈퇴 기능
+
+	// 회원 탈퇴 기능
 	public boolean delete(String id, String pw) throws Exception {
 		Connection con = JdbcUtils.connect();
-	
-		String sql="delete member where id = ? and pw = ?";
+
+		String sql = "delete member where id = ? and pw = ?";
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, id);
 		ps.setString(2, pw);
 		int result = ps.executeUpdate();
-	
+
 		con.close();
-	
+
 		return result > 0;
-}
-	//목록 조회 기능
+	}
+
+	// 목록 조회 기능
 	public List<MemberDto> list() throws Exception {
 		Connection con = JdbcUtils.connect();
-		
-		String sql="select * from member ";
+
+		String sql = "select * from member ";
 		PreparedStatement ps = con.prepareStatement(sql);
 		ResultSet rs = ps.executeQuery();
-		
+
 		List<MemberDto> memberList = new ArrayList<>();
-		while(rs.next()) {
+		while (rs.next()) {
 			MemberDto memberDto = new MemberDto();
-			
+
 			memberDto.setId(rs.getString("id"));
 			memberDto.setPw(rs.getString("pw"));
 			memberDto.setName(rs.getString("name"));
@@ -64,15 +65,15 @@ public class MemberDao {
 			memberDto.setPoint(rs.getInt("point"));
 			memberDto.setGrade(rs.getString("grade"));
 			memberDto.setGender(rs.getString("gender"));
-			
+
 			memberList.add(memberDto);
 		}
 		con.close();
-		
+
 		return memberList;
-		}
-	
-	//회원상세 기능
+	}
+
+	// 회원상세 기능
 	public MemberDto get(String id) throws Exception {
 		Connection con = JdbcUtils.connect();
 		
@@ -104,5 +105,65 @@ public class MemberDao {
 		con.close();
 		
 		return memberDto;
+		
+		
+		// 개인정보 변경 메소드
+		public boolean edit(MemberDto memberDto) throws Exception { 
+			Connection con = JdbcUtils.connect();
+			
+			String sql = "update member" + "set"	+ "member_pw = ?, "
+													+ "member_email = ?, "
+													+ "member_phone = ?, "
+													+ "member_birth = to_date(?, 'YYYY-MM-DD'), "
+													+ "where" + "member_id = ? and member_pw = ?";
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, memberDto.getMemberPw);
+			ps.setString(2, memberDto.getMemberEmail);
+			ps.setString(3, memberDto.getMemberPhone);
+			ps.setString(4, memberDto.getMemberBirth);
+			ps.setString(5, memberDto.getMemberId);
+			ps.setString(6, memberDto.getMemberPw);
+			int result = ps.executeUpdate();
+			
+			con.close();
+			
+			return result > 0;
+			
+		
+		}
+		
+		
+		// 비밀번호 변경 메소드
+		public boolean editPassword(String memberId, String memberPw, String changePw) throws Exception {
+			Connection con = JdbcUtils.connect();
+			
+			String sql = "update member set member_pw = ? where member_id = ? and member_pw = ?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, changePw);//바꿀비밀번호
+			ps.setString(2, memberId);//현재아이디
+			ps.setString(3, memberPw);//현재비밀번호
+			int result = ps.executeUpdate();
+			
+			con.close();
+			
+			return result > 0;
+		
+		
+		// 회원탈퇴 메소드
+			public boolean quit(String memberId) throws Exception {
+				Connection con = JdbcUtils.connect2();
+
+				String sql = "delete member where member_id = ?";
+				PreparedStatement ps = con.prepareStatement(sql);
+				ps.setString(1, memberId);
+				int result = ps.executeUpdate();
+				
+				con.close();
+				
+				return result > 0;
+			}
+			
 	}
+}
 }
