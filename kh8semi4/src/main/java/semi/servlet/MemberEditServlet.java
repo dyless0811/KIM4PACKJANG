98e821269e16bd57d7b91349e0102f633b87e391
lostMS95 <rmsdn0415@naver.com>
2021-11-16 01:38:46 +0900

    회원 개인정보변경 servlet 추가

---
+++ kh8semi4/src/main/java/semi/servlet/MemberEditServlet.java
@@ -0,0 +1,46 @@
+package semi.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import semi.beans.MemberDao;
+import semi.beans.MemberDto;
+
+@WebServlet(urlPatterns = "/member/edit.kj")
+public class MemberEditServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			// 입력 : MemberDto
+			// = 세션 : id (loginId)
+			MemberDto memberDto = new MemberDto();
+			memberDto.setId((String) req.getSession().getAttribute("loginId"));// 세션
+			memberDto.setPw(req.getParameter("pw"));// 파라미터
+			memberDto.setName(req.getParameter("name"));// 파라미터
+			memberDto.setAddress(req.getParameter("address"));// 파라미터
+			memberDto.setPhone(req.getParameter("phone"));// 파라미터
+			memberDto.setEmail(req.getParameter("email"));// 파라미터
+			memberDto.setBirth(req.getParameter("birth"));// 파라미터
+			memberDto.setGender(req.getParameter("gender"));// 파라미터
+
+			// 처리
+			MemberDao memberDao = new MemberDao();
+			boolean success = memberDao.edit(memberDto);
+
+			// 출력
+			if (success) {
+				resp.sendRedirect("edit.jsp");
+			} else {
+				resp.sendRedirect("edit.jsp?error");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
