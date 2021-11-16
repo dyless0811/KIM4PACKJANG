1deff5290e61e098ae7f30a70abcb8a4398bb11c
hongdyun9410 <dkwntbvj@naver.com>
2021-11-16 15:26:53 +0900

    product servlet jsp수정

--- kh8semi4/src/main/java/semi/beans/ProductDao.java
+++ kh8semi4/src/main/java/semi/beans/ProductDao.java
@@ -29,7 +29,7 @@ public class ProductDao {
 	public boolean update(ProductDto productDto) throws Exception{
 		Connection con= JdbcUtils.connect();
 		
-		String sql="update product set smallTypeNo=?,name=?,price=?,description=? where no=?";
+		String sql="update product set small_type_no=?,name=?,price=?,description=? where no=?";
 		
 		PreparedStatement ps =con.prepareStatement(sql);
 		ps.setInt(1, productDto.getSmallTypeNo());
--- kh8semi4/src/main/java/semi/servlet/product/ProductEditServlet.java
+++ kh8semi4/src/main/java/semi/servlet/product/ProductEditServlet.java
@@ -10,25 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import semi.beans.ProductDao;
 import semi.beans.ProductDto;
-@WebServlet (urlPatterns = "/#asdasdasd")
+@WebServlet (urlPatterns = "/product/edit.kj")
 public class ProductEditServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
 			//입력
 			ProductDto productDto = new ProductDto();
-			productDto.setSmallTypeNo(Integer.parseInt(req.getParameter("SamllTypeNo")));
+			productDto.setNo(Integer.parseInt(req.getParameter("no")));
+			productDto.setSmallTypeNo(Integer.parseInt(req.getParameter("SmallTypeNo")));
 			productDto.setName(req.getParameter("name"));
 			productDto.setPrice(Integer.parseInt(req.getParameter("price")));
 			productDto.setDescription(req.getParameter("description"));
 			//처리
+			
 			ProductDao productDao = new ProductDao();
 			boolean success=productDao.update(productDto);
+			
 			//출력
 			if(success) {
-				resp.sendRedirect("");
+				resp.sendRedirect("productdetail.jsp?no="+productDto.getNo());
 			}else {
-				resp.sendRedirect("");
+				resp.sendError(500);
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
