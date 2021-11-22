edbbce1953d8ba014a9159aab4f74230e244485f
dyless0811 <dyless0811@gmail.com>
2021-11-22 10:41:38 +0900

    장바구니 Vo 추가
    - basketDao.voListByMemberId

---
+++ kh8semi4/src/main/java/semi/beans/BasketVo.java
@@ -0,0 +1,80 @@
+package semi.beans;
+
+public class BasketVo {
+	private String productFileSavename;
+	private int productNo;
+	private String productName;
+	private String colorName;
+	private String sizeName;
+	private int price;
+	private int basketNo;
+	private int count;
+	
+	public BasketVo() {
+		super();
+	}
+
+	public String getProductFileSavename() {
+		return productFileSavename;
+	}
+
+	public void setProductFileSavename(String productFileSavename) {
+		this.productFileSavename = productFileSavename;
+	}
+
+	public int getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getColorName() {
+		return colorName;
+	}
+
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+
+	public String getSizeName() {
+		return sizeName;
+	}
+
+	public void setSizeName(String sizeName) {
+		this.sizeName = sizeName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getBasketNo() {
+		return basketNo;
+	}
+
+	public void setBasketNo(int basketNo) {
+		this.basketNo = basketNo;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
