a32ef2000ccdbb3464065ae93de5e3cdf54378ab
dyless0811 <dyless0811@gmail.com>
2021-11-25 11:18:46 +0900

    productdetail 리뷰 추가

---
+++ kh8semi4/src/main/java/semi/beans/ReplyVo.java
@@ -0,0 +1,69 @@
+package semi.beans;
+
+public class ReplyVo {
+	private int no;
+	private int productNo;
+	private String productName;
+	private String colorName;
+	private String sizeName;
+	private String content;
+	private String memberId;
+	private int starpoint;
+	private String replySavename;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getColorName() {
+		return colorName;
+	}
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+	public String getSizeName() {
+		return sizeName;
+	}
+	public void setSizeName(String sizeName) {
+		this.sizeName = sizeName;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public int getStarpoint() {
+		return starpoint;
+	}
+	public void setStarpoint(int starpoint) {
+		this.starpoint = starpoint;
+	}
+	public String getReplySavename() {
+		return replySavename;
+	}
+	public void setReplySavename(String replySavename) {
+		this.replySavename = replySavename;
+	}
+
+
+}
